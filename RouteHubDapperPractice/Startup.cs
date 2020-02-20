7a411d8df8094544cb32aa30a4a08ff4a55e6d38
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2020-02-20 18:55:35 -0500

    Made upcoming default page

--- RouteHubDapperPractice/Startup.cs
+++ RouteHubDapperPractice/Startup.cs
@@ -54,7 +54,7 @@ namespace RouteHubDapperPractice
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Upcoming}/{action=Index}/{id?}");
             });
         }
     }
