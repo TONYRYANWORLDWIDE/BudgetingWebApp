2c82320c5a0e0e469c9b6ab14e61d33911c26d6c
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2020-02-22 23:12:48 -0500

    added data validation to monthlybill model

--- MonthlyBillsWithDapper/Entity/MonthlyBill.cs
+++ MonthlyBillsWithDapper/Entity/MonthlyBill.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MonthlyBillsWithDapper.Entity
 {
     public class MonthlyBill
     {
         public int id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Bill")] 
         public string Bill { get; set; }
+        [Required]
+        [Display(Name = "Cost")]
         public Nullable<float> Cost { get; set; }
+        [Required]
+        [Range(0, 31)] 
         public Nullable<int> Date { get; set; }
         public string BillAlias { get; set; }
         public string UserID { get; set; }
